51dab390f7c92ae9470e00805b1ecf91b8427199
newtondotcom <asphalt9fr@gmail.com>
2023-10-09 21:34:06 +0200

    fix : correct authentification callback link

--- oauth/settings.py
+++ oauth/settings.py
@@ -3,20 +3,12 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-3^y46g&vyc6^nkz8h&fou0#vou6$a^wo(4e)xbhfz5q4u^nphy"
 
 ALLOWED_HOSTS = ["*"]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -70,9 +62,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
@@ -81,8 +70,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
@@ -110,7 +97,7 @@ CORS_ALLOW_HEADERS = [
 'x-requested-with',
 ]
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 if DEBUG:
