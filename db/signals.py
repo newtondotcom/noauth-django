b7cc07167fc83acce28396ef4f24adf667ef0076
newtondotcom <asphalt9fr@gmail.com>
2024-03-05 14:24:38 +0100

    create signals on currentBot edit

--- db/signals.py
+++ db/signals.py
@@ -1,16 +1,21 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from db.models import *
+from oauth.docker import *
+from oauth.subscriptions import *
 
 # Define your function to execute when a row is added
 @receiver(post_save, sender=CurrentBots)
 def handle_row_added(sender, instance, created, **kwargs):
     if created:
-        # Code to execute when a row is added
-        print("A row was added to MyModel")
+        fetch_docker_master()
 
 # Define your function to execute when a row is deleted
 @receiver(post_delete, sender=CurrentBots)
 def handle_row_deleted(sender, instance, **kwargs):
-    # Code to execute when a row is deleted
-    print("A row was deleted from MyModel") 
+    fetch_docker_master()
+
+@receiver(post_save, sender=Payment)
+def handle_row_added(sender, instance, created, **kwargs):
+    if created:
+        added_payment(instance.id)
