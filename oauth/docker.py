3dc68dc80ce3c92111b4eefba1ef8be4c278813d
newtondotcom <asphalt9fr@gmail.com>
2024-03-05 15:12:14 +0100

    beginning of reach docker master

--- oauth/docker.py
+++ oauth/docker.py
@@ -1,7 +1,12 @@
 import requests
+from db.models import *
 
 def fetch_docker_master():
     url = "http://localhost:3000/health"
     response = requests.get(url)
     if not response.status_code == 200:
         print("Docker master is down")
+
+    # updateList
+    # bots = CurrentBots.objects.all()
+    # body = { }
