90398ccbf5bbad51f635088f51a53db84abc4e72
newtondotcom <asphalt9fr@gmail.com>
2024-03-05 14:25:57 +0100

    functions to check payments

--- oauth/subscriptions.py
+++ oauth/subscriptions.py
@@ -10,8 +10,11 @@ def check_payments():
             if  payment.date + datetime.timedelta(days=payment.duration) < date:
                 payment.is_over = True
                 payment.save()
-        if Payment.objects.filter(buyer=bot.bot, is_over=False).exists():
-            if not CurrentBots.objects.filter(bot=bot.bot).exists():
-                CurrentBots.objects.create(bot=bot.bot).stave()
-        else:
+        if not Payment.objects.filter(buyer=bot.bot, is_over=False).exists():
             CurrentBots.objects.filter(bot=bot.bot).delete()
+
+def added_payment(id_buying):
+    payment = Payment.objects.get(id=id_buying)
+    bot = payment.buyer
+    if not CurrentBots.objects.filter(bot=bot).exists():
+        CurrentBots.objects.create(bot=bot)
