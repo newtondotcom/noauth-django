d847f114fa9ab84b723cccbe7b2f2bf6c1bf09f5
newtondotcom <asphalt9fr@gmail.com>
2024-03-05 14:41:24 +0100

    localhost docker deploy test connection

--- oauth/docker.py
+++ oauth/docker.py
@@ -1,9 +1,7 @@
 import requests
 
 def fetch_docker_master():
-    url = "http://localhost:8000"
+    url = "http://localhost:3000/health"
     response = requests.get(url)
-    if response.status_code == 200:
-        return "Success"
-    else:
-        return None
+    if not response.status_code == 200:
+        print("Docker master is down")
