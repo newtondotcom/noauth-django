a7d0ae144dc09c2964e6ef1ad624b8293e1ab254
newtondotcom <asphalt9fr@gmail.com>
2024-03-16 16:49:42 +0100

    get_params enableds api key verif

--- oauth/middleware.py
+++ oauth/middleware.py
@@ -5,7 +5,7 @@ from db.models import *
 class ApiKeyMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.api_key_allowed_endpoints = ['','get_ip_master','get_params']
+        self.api_key_allowed_endpoints = ['']
         print("Api Key Middleware initialized") 
 
     def __call__(self, request):
@@ -16,17 +16,11 @@ class ApiKeyMiddleware:
             response = self.get_response(request)
             return response
 
-
         if not cleanedPath.count("verif") > 0 and not cleanedPath.count("admin") > 0 and cleanedPath.count("test_users") > 0 and cleanedPath not in self.api_key_allowed_endpoints:
             if api_key_header and api_key_header.startswith('Api-Key '):
                 api_key = api_key_header.split(' ')[1]
                 keys = ["69f13396-66d1-4736-873e-0ddd2ba476ea","f3e3e3e3-66d1-4736-873e-0ddd2ba476ea"]
-                KeyCorrect = False
-                for k in keys:
-                    if k == api_key:
-                        KeyCorrect = True
-                        break
-                if not KeyCorrect:
+                if keys.count(api_key) == 0:
                     return JsonResponse({'error': 'Invalid API key'}, status=403)
 
             else:
