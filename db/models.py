9c1f92690ede80db559195906c9a28f7baa0c8c4
newtondotcom <asphalt9fr@gmail.com>
2024-03-19 19:11:57 +0100

    rm useles class + deleted field

--- db/models.py
+++ db/models.py
@@ -22,19 +22,6 @@ class DiscordServerJoined(models.Model):
     master = models.ForeignKey(Bots, on_delete=models.CASCADE)
     guild_id = models.CharField(max_length=60)
     roleToGiveVerif = models.CharField(max_length=60, null=True)
-
-
-## Discord Users registered
-class DiscordUsers(models.Model):
-    userID = models.CharField(max_length=40, null=True)
-    access_token = models.CharField(max_length=300)
-    refresh_token = models.CharField(max_length=300, null=True)
-    username = models.CharField(max_length=50)
-    email = models.CharField(max_length=150, null=True)  
-    server_guild = models.ForeignKey(DiscordServerJoined, on_delete=models.CASCADE, null=True, blank=True)
-
-    def __str__(self):
-        return self.username
     
 class NoAuthUsers(models.Model):
     userID = models.CharField(max_length=40, null=True)
@@ -43,6 +30,7 @@ class NoAuthUsers(models.Model):
     username = models.CharField(max_length=50)
     email = models.CharField(max_length=150, null=True)  
     master = models.ForeignKey(Bots, on_delete=models.CASCADE, null=True, blank=True)
+    deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
